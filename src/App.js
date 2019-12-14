17b164cccce073c829e4b65da3efc881d211c72e
erees <erees@protonmail.com>
2019-12-14 17:05:01 -0500

    First commit. Setting up divs and simple fetch of data.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  
+  const getPeople = (e) => {
+    e.preventDefault();
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(json => setPeople(json))
+      .then(console.log(people))
+      .catch(err => console.error({ message: err }))
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Sidebar">
+        <button className="getButton" onClick={(e) => getPeople(e)}>GET</button>
+        <button className="addButton">ADD</button>
+        <button className="sortUpButton">SORT UP</button>
+        <button className="sortDownButton">SORT DOWN</button>
+      </div>
+      <div className="Results">
+        {people.map((person, index) => {
+          return(
+            `${person.id} , ${person.name} , ${person.username} `
+             
+          )
+        })}
+      </div>
     </div>
   );
 }
