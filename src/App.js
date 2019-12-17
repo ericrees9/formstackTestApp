4aac20a9a0316760df0bfda60e56b0a5b217c95d
erees <erees@protonmail.com>
2019-12-16 20:22:15 -0500

    Delete function is working.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'react-foundation';
 import PeopleCard from './components/PeopleCard/PeopleCard.js';
+import { nextTick } from 'q';
 
 function App() {
   const [people, setPeople] = useState([]);
@@ -31,6 +32,7 @@ function App() {
     })
     .then(response => response.json())
     .then(json => console.log(json))
+    .catch(err => console.error({ message: err }))
   }
 
   const sortPeopleUp = (e) => {
@@ -48,6 +50,44 @@ function App() {
     setCount(count - 1);
     console.log(people);
   }
+
+  const deleteArray = (id) => {
+    // console.log(`Running in deleteArray function. The id is: ${id}`);
+
+    // let minusOne = people.splice(id-1,1);
+    // setPeople(people);
+    // setCount(count + 1);
+    // console.log(people);
+    
+    // console.log(`The arrayID is: ${id-1}`);
+
+    // for( let i = 0; i < people.length; i++ ){ 
+    //   if (people[i] === id-1) {
+    //     console.log(`the loop is running.`);
+    //     people.splice(i, 1);
+    //     i--;
+    //     setPeople(people);
+    //     setCount(count + 1); 
+    //     console.log(people);
+    //   }
+    // }
+
+    // people.forEach((e) => {
+    //   // console.log(e);
+    //   // console.log(id);
+    //   if (e.id == id) {
+    //     console.log(`success!`)
+    //     let minus = people.splice(id-1, 1);
+    //     setPeople(people);
+    //     setCount(count + 1); 
+    //   }
+    // });
+
+    let minus = people.splice(people.findIndex(e => e.id === id),1);
+    setPeople(people);
+    setCount(count + 1);
+    console.log(people);
+  }
   
   return (
     <div className="App">
@@ -60,7 +100,9 @@ function App() {
       <div className="Results">
         {people.map((person, index) => {
           return(
-            <PeopleCard personData={person}></PeopleCard>
+            // passing the individual person data as props to the component for data to render
+            // passing in the getPeople function so that the deletePeople function in the component can refresh the array
+            <PeopleCard personData={person} deleteArray={deleteArray}></PeopleCard>
           )
         })}
       </div>
--- src/components/PeopleCard/PeopleCard.js
+++ src/components/PeopleCard/PeopleCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../PeopleCard/PeopleCard.css';
 import { Button, Callout, Colors, Switch, Sizes } from 'react-foundation';
 
 const PeopleCard = (props) => {
-    
     // console.log(props);
+
+    const deletePeople = () => {
+        // let getPeople = props.getPeople;
+
+        // fetch(`https://jsonplaceholder.typicode.com/users/${props.personData.id}`, {
+        //     method: 'DELETE'
+        // })
+        // .then(getPeople(e))
+        // .then(console.log('This person has been deleted.'))
+        // .catch(err => console.error({ message: err }))
+        console.log(`The ID number is: ${props.personData.id}`);
+        props.deleteArray(props.personData.id);
+    }
 
     return(
         <Callout className="mainCard" color={Colors.SECONDARY}>
@@ -16,7 +28,7 @@ const PeopleCard = (props) => {
             </div>
             <div className="actionArea">
                 <Switch></Switch>
-                <Button color={Colors.ALERT}>Delete</Button>
+                <Button color={Colors.ALERT} onClick={(e) => deletePeople(e)}>Delete</Button>
             </div>
         </Callout>
     );
